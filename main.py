d18b246d8c4690d5f74df5285df21fc6b3207e7f
DepyXa <106472263+mrDepyXa@users.noreply.github.com>
2022-05-30 22:10:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
+import defer
 
 print("Hello World")
-#OpenenPut = input("Specify the location of the file:")
 while True:
 	DIA = input("PySestem > ")
+	if DIA == "open_file":
+		print("Locate the file...")
+		pute = input("PySestem:/User/")
+		defer.open_file()
+	elif DIA == "create_new_file":
+		print("""Specify an existing folder, followed by a file with the format.
+For example: (PySestem:/User/pack/file.fomat)""")
+		pute = input("PySestem:/User/")
+		defer.create_new_file()
+	else:
+		print(f""""{DIA}" the command does not exist in the pysestem!""")
